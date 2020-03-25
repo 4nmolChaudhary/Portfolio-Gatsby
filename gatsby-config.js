d036fbd7abe2cb5d10a0407a8503d60895d33a98
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2020-03-25 19:56:34 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,21 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `Anmol Chaudhary - UI/UX Designer & Developer`,
+    author: {
+      name: `Anmol Chaudhary`,
+      summary: `Portfolio`,
+    },
     siteUrl: `https://www.gatsbyjs.org`,
     description: `Blazing fast modern site generator for React`,
-  }
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Anmol Chaudhary - UI/UX Designer & Developer`,
+        icon: `static/favicon.png`
+      }
+    }
+  ]
 }
