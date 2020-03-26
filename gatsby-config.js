2b64e936659123b0d77f6c39bb09e1cb14142872
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2020-03-26 21:18:12 +0530

    new

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,27 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `Anmol Chaudhary - UI/UX Designer & Developer`,
+    author: {
+      name: `Anmol Chaudhary`,
+      summary: `Portfolio`,
+    },
     siteUrl: `https://www.gatsbyjs.org`,
     description: `Blazing fast modern site generator for React`,
-  }
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/favicon.png`
+      }
+    }
+  ]
+
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import '../styles/main.css'
 import Home from '../components/Home'
 import SideNav from '../components/SideNav'
+import Helmet from "react-helmet"
 function index() {
     return (
         <div>
+            <Helmet>
+                <title>Anmol Chaudhary - UI/UX Designer & Developer</title>
+            </Helmet>
             <SideNav page="home" />
             <Home />
         </div>
